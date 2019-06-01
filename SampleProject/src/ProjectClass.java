753b01c5275954d021f0c1b7321656f6ab0c5536
HP User <HP User@192.168.1.11>
2019-06-02 00:00:57 +0530

    commiting

--- SampleProject/src/ProjectClass.java
+++ SampleProject/src/ProjectClass.java
@@ -1,6 +1,6 @@
 
 public class ProjectClass { 
 	    public static void main(String args[]){  
-	     System.out.println("Hello Java");  
+	     System.out.println("Hello Javakjgk");  
 	    }  
 	} 
